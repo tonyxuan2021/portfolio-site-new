7d18e9d7c79d89b8e51727c05a3a272907e4d0a9
tonyxuan2021 <tonyyexuan@gmail.com>
2022-08-13 09:04:52 -0700

    complete about me section

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,29 +1,48 @@
 import { Button, Grid, Typography } from "@mui/material";
 import React from "react";
 import photo from "../assets/Xuan Ye.jpg";
+import { theme } from "../theme";
 
 const Content = () => {
   return (
     <Grid
       container
       display="flex"
-      justifyContent="space-between"
+    //   justifyContent="space-between"
+    gap={8}
       alignItems="center"
+      sx={{ height: "70vh" }}
     >
-      <Grid item xs={5}>
-        <img
-          style={{ width: "100%", maxHeight: "65vh", objectFit: "cover" }}
-          src={photo}
-        ></img>
+      <Grid item xs={5.5}>
+        <Grid item>
+          <img
+            style={{
+              width: "100%",
+              height: "70vh",
+              objectFit: "cover",
+              objectPosition: "top",
+            }}
+            src={photo}
+          ></img>
+        </Grid>
       </Grid>
-      <Grid item xs={5} sx={{ p: 8 }}>
-        <Typography variant="h4">About me</Typography>
-        <Typography textAlign="justify">
-          {/* Welcome! My name is Tony Ye, I am a Web Development Bootcamp graduate
-          loacted in Vancouver, Canada, and I am currently making a career
-          transition from an accountant to a full stack web developer. Beside
-          coding, I enjoy gardening and playing with my pets. If you are a
-          business looking to hire, contact me here. */}
+      <Grid
+        item
+        xs={5}
+        sx={{
+          height: "100%",
+          borderTop: "1px solid lightgrey",
+          orderBottom: "1px solid lightgrey",
+          p: 3,
+        }}
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-between"
+      >
+        <Typography variant="h4" fontWeight={700}>
+          About Me
+        </Typography>
+        <Typography textAlign="justify" variant="body1" lineHeight={2}>
           I've enjoyed my 5-year career as an accountant. I am wired to help
           others, and to provide prudential consultation to solve complex
           financial issues. Now, I am exited to pivot my career from the world
@@ -35,10 +54,17 @@ const Content = () => {
           knowledge. I love coding and I look forward to connecting with you
           about how we can work together!
         </Typography>
-        <Button>
-          <Typography sx={{ color: "black", background: "pink" }} variant="contained">
-            GO TO PORTFOLIO
-          </Typography>
+        <Button
+          sx={{
+            borderRadius: 0,
+            color: theme.palette.secondary.main,
+            border: `1px solid  ${theme.palette.secondary.main}`,
+            width: "60%",
+            p:2
+          }}
+          variant="outlined"
+        >
+          <Typography variant="body2">GO TO PORTFOLIO</Typography>
         </Button>
       </Grid>
     </Grid>
