ee8e064c83276a6276589c5a1e4af182f2df1726
tonyxuan2021 <tonyyexuan@gmail.com>
2022-08-14 13:46:33 -0700

    complete portfolio page tablet

--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -5,36 +5,68 @@ import amazon from "../assets/portfolio/amazon.jpg";
 import book from "../assets/portfolio/book.jpg";
 import dash from "../assets/portfolio/dash.jpg";
 import { theme } from "../theme";
+import { styled } from "@mui/material/styles";
+
+const PortfolioWrapper = styled(Grid)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  marginBottom: 80,
+  [theme.breakpoints.up("md")]: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 100,
+  },
+}));
+
+const PageWrapper = styled(Grid)(({ theme }) => ({
+  paddingLeft: 30,
+  paddingRight: 30,
+  [theme.breakpoints.up("md")]: {
+    paddingLeft: 100,
+    paddingRight: 100,
+  },
+}));
+
+const PortfolioLeft = styled(Grid)(({ theme }) => ({
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+  height: "38vh",
+  [theme.breakpoints.up("md")]: {
+    marginBottom: 0,
+    height: "32vh",
+  },
+}));
+
+const PortfolioRight = styled(Grid)(({ theme }) => ({
+  paddingTop: 30,
+  paddingBottom: 30,
+  marginTop: 30,
+  borderTop: "1px solid lightgrey",
+  borderBottom: "1px solid lightgrey",
+  flexDirection: "column",
+  [theme.breakpoints.up("md")]: {
+    marginTop: 0,
+    paddingTop: 50,
+    paddingBottom: 50,
+    minHeight: 536,
+    justifyContent: "center",
+  },
+}));
 
 const PortfolioPage = () => {
   return (
-    <Grid item sx={{ pl: 3, pr: 3 }}>
+    <PageWrapper item>
       {projects.map((project) => {
         return (
-          <Grid item sx={{ mb: 8 }}>
-            <Grid
+          <PortfolioWrapper container>
+            <PortfolioLeft
               item
-              sx={{
-                backgroundImage: `url(${project.image})`,
-                backgroundPosition: "center",
-                backgroundSize: "cover",
-                height: "38vh",
-                mb: 5,
-              }}
-            ></Grid>
-            <Grid
-              item
-              display="flex"
-              flexDirection="column"
-              gap={3}
-              sx={{
-                pt: 3,
-                pb: 3,
-                mt: 3,
-                borderTop: "1px solid lightgrey",
-                borderBottom: "1px solid lightgrey",
-              }}
-            >
+              md={5.5}
+              sx={{ backgroundImage: `url(${project.image})` }}
+              order={project.order}
+            ></PortfolioLeft>
+            <PortfolioRight item display="flex" gap={3} md={5.5}>
               <Typography variant="h3" fontWeight={700}>
                 {project.name}
               </Typography>
@@ -61,11 +93,11 @@ const PortfolioPage = () => {
               >
                 <Typography>VIEW PROJECT</Typography>
               </Button>
-            </Grid>
-          </Grid>
+            </PortfolioRight>
+          </PortfolioWrapper>
         );
       })}
-    </Grid>
+    </PageWrapper>
   );
 };
 
@@ -74,16 +106,19 @@ const projects = [
     image: amazon,
     name: "Amazon.com clone",
     desc: "Built clone of Amazon.com in a team of two that allow users to browse products, add items to cart and then to purchase. This project incorporated Redux hooks with Redux Toolkit for frontend data process and storage, it also used Prisma as primary ORM library for CRUD operations with PostgreSQL database.",
+    order: 0,
   },
   {
     image: book,
     name: "The Booktown",
     desc: "This e-commerce, full-stack web application allows the users to search and to purchase their favoriate books.",
+    order: 1,
   },
   {
     image: dash,
     name: "Diamond Admin PRO",
     desc: "This web application is a UI focused, analytics user platform.",
+    order: 0,
   },
 ];
 
