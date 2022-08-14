7ebb617a22f884cd63ce1462e25195d2cbcdc114
tonyxuan2021 <tonyyexuan@gmail.com>
2022-08-14 11:13:48 -0700

    complete home page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,38 +8,51 @@ import logo from "../assets/logobtm.svg";
 import { Grid } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { styled } from "@mui/material/styles";
+
+const CustomFooter = styled(Toolbar)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between",
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column",
+    gap: 30,
+  },
+}));
+
+const CustomGrid = styled(Grid)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column",
+  },
+}));
+
+const CustomAppbar = styled(AppBar)(({ theme }) => ({
+  background: theme.palette.secondary.main,
+  boxShadow: "none",
+  paddingLeft: 70,
+  paddingRight: 70,
+
+  [theme.breakpoints.down("sm")]: { paddingTop: 70, paddingBottom: 70 },
+}));
 
 export default function Footer() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar
-        position="static"
-        sx={{
-          background: theme.palette.secondary.main,
-          boxShadow: "none",
-          pl: 7,
-          pr: 7,
-        }}
-      >
-        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-          <Grid
-            item
-            display="flex"
-            alignItems="center"
-            gap={5}
-            sx={{ color: theme.palette.grey.main }}
-          >
+      <CustomAppbar position="static">
+        <CustomFooter>
+          <CustomGrid item gap={5} sx={{ color: theme.palette.grey.main }}>
             <img src={logo}></img>
             <Typography>HOME</Typography>
             <Typography>PORTFOLIO</Typography>
             <Typography>CONTACT ME</Typography>
-          </Grid>
+          </CustomGrid>
           <Grid item display="flex" gap={2}>
             <GitHubIcon fontSize="large" sx={{ color: "white" }} />
             <LinkedInIcon fontSize="large" sx={{ color: "white" }} />
           </Grid>
-        </Toolbar>
-      </AppBar>
+        </CustomFooter>
+      </CustomAppbar>
     </Box>
   );
 }
