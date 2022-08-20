27a951e444a9dfb5680edcfdd0640399b94720e0
tonyxuan2021 <tonyyexuan@gmail.com>
2022-08-20 16:30:12 -0700

    update resume link

--- src/components/Header.js
+++ src/components/Header.js
@@ -97,7 +97,7 @@ export default function Header() {
             })}
             <a
               target="_blank"
-              href="https://drive.google.com/file/d/1Qe4lthwjK6yGc-16sPJc8WwKMKdBXkm_/view?usp=sharing"
+              href="https://drive.google.com/file/d/1l22KqBFb1feGBJHvLHmqxfHglhdjEcIW/view?usp=sharing"
             >
               <NavText variant="body2" color={theme.palette.secondary.main}>
                 RESUME
